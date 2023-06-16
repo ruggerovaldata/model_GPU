0cc9c4efebffc0ba006aa4d3d8c40579655e618a
ruggerovaldata <99249961+ruggerovaldata@users.noreply.github.com>
2023-06-16 18:57:31 +0200

    .

--- test.py
+++ test.py
@@ -39,7 +39,7 @@ print(device)
 
 if load: 
     print('Loading the model')
-    model.load_state_dict(torch.load('FRBAEGPU10160617:18.pt',map_location=torch.device(device)))
+    model.load_state_dict(torch.load('FRBAEGPU10160618:54.pt',map_location=torch.device(device)))
 
 model.to(device)
 
