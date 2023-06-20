aea96598cd874f3c83cf73443a4cd61960f4aa11
ruggerovaldata <99249961+ruggerovaldata@users.noreply.github.com>
2023-06-20 18:31:18 +0200

    .

--- model_def.py
+++ model_def.py
@@ -196,7 +196,7 @@ def train(model, epochs, train_dataloader, testing_dataloader, optimizer):
             
     return model
 
-class VariationalAutoEncoder_noswidth_RealNoise(nn.Module):
+""" class VariationalAutoEncoder_noswidth_RealNoise(nn.Module):
   def __init__(self,input_dim,x_size,y_size, nu, nu0,t, h_dim = 100, h1_dim = 50, h2_dim = 50, z_dim_params = 2, z_dim_noise = 1000):
 
     super(VariationalAutoEncoder_noswidth_RealNoise,self).__init__()
@@ -321,7 +321,84 @@ def train(model, epochs, train_dataloader, testing_dataloader, optimizer):
                     test_loss+=test_loss1+loss_function(x-x_hat_params,x_hat_noise, model.y_size, model.x_size)
                 print(f'===>Test loss: {(test_loss-test_loss1):.4f}')
             
-    return model
+    return model """
+
+class VariationalAutoEncoder_noswidth_RealNoise(nn.Module):
+  def __init__(self,input_dim,x_size,y_size, nu, nu0,t, h_dim = 100, h1_dim = 50, h2_dim = 50, z_dim_params = 2, z_dim_noise = 1000):
+
+    super(VariationalAutoEncoder_noswidth_RealNoise,self).__init__()
+    
+    #encoder
+    self.img_2hid = nn.Linear(input_dim, h_dim)
+
+    self.first_layer = nn.Linear(h_dim,h1_dim)
+    self.second_layer = nn.Linear(h1_dim, h2_dim)
+    
+    self.hid_2mu_params = nn.Linear(h2_dim, z_dim_params)
+    self.hid_2sigma_params = nn.Linear(h2_dim, z_dim_params)
+
+    self.hid_2mu_noise = nn.Linear(h2_dim, z_dim_noise)
+    self.hid_2sigma_noise = nn.Linear(h2_dim, z_dim_noise)
+
+    #decoder: 
+    self.zparams_2img = nn.Linear(z_dim_params, input_dim)
+
+    self.hidnoise_2hid = nn.Linear(z_dim_noise,h2_dim)
+    self.first_decoder_layer = nn.Linear(h2_dim,h1_dim)
+    self.second_decoder_layer = nn.Linear(h1_dim,h_dim)
+    self.hid_2img = nn.Linear(h_dim, input_dim)
+
+    self.relu = nn.ReLU()
+
+    #Variables for decoder and forward pass 
+    self.input_dim = input_dim
+    self.nu = nu 
+    self.nu0 = nu0
+    self.t = t
+    self.x_size = x_size
+    self.y_size = y_size
+
+
+  def encode(self,x):
+    h1 = self.relu(self.img_2hid(x))
+    h2 = self.relu(self.first_layer(h1))
+    h = self.relu(self.second_layer(h2))
+    mu_params, sigma_params = self.hid_2mu_params(h), self.hid_2sigma_params(h)
+    mu_noise, sigma_noise = self.hid_2mu_noise(h), self.hid_2sigma_noise(h)
+    return mu_params,sigma_params, mu_noise, sigma_noise
+  
+  def decodeparams(self,z):
+    dm_pred= z[:,0]
+    swidth_pred = z[:,1]
+    device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+    dm_pred = 10*dm_pred.view(dm_pred.shape[0])
+    #swidth_pred = swidth_pred.view(swidth_pred.shape[0])
+    swidth_pred = 1000*torch.abs(swidth_pred)
+    #print(swidth_pred)
+    width =  0.000001*torch.ones(len(dm_pred))
+    width = width.to(device)
+    out = s.decoder_noswidth(dm_pred, width,self.nu,self.nu0,self.t,swidth_pred,self.x_size,self.y_size,plot_flag=False)
+    #out_image = out[:,60:110,:]
+    return out, [dm_pred,swidth_pred]
+  
+  def decodenoise(self,z):
+    h1 = self.relu(self.hidnoise_2hid(z))
+    h2 = self.relu(self.first_decoder_layer(h1))
+    h = self.relu(self.second_decoder_layer(h2))
+    img = self.hid_2img(h)
+    return img.view(-1,self.y_size,self.x_size)
+
+  def forward(self, x):
+    mu_params, logvar_params, mu_noise,logvar_noise = self.encode(x.view(-1,self.input_dim))
+    std_params = 0.5*torch.exp(logvar_params-32)
+    std_noise = 0.5*torch.exp(logvar_noise-32)
+    epsilon_params = std_params.data.new(std_params.size()).normal_()
+    epsilon_noise = std_noise.data.new(std_noise.size()).normal_()
+    z_reparametrized_params = mu_params + std_params * epsilon_params
+    z_reparametrized_noise = mu_noise + std_noise * epsilon_noise
+    x_reconstructed_params, params = self.decodeparams(z_reparametrized_params)
+    x_reconstructed_noise = self.decodenoise(z_reparametrized_noise)
+    return x_reconstructed_params, x_reconstructed_noise, params
 
 def save_model(model,batch_size):
   from datetime import datetime
